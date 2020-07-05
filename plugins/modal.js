4f158110867cb74f5f572b533a9aec3bcd21f67d
Filipp <fili92@mail.ru>
2020-07-05 20:21:47 +0300

    basic operations finished

--- plugins/modal.js
+++ plugins/modal.js
@@ -3,11 +3,11 @@ function _createModal(options) {
     const modal = document.createElement('div')
     modal.classList.add('fmodal')
     modal.insertAdjacentHTML('afterbegin', `
-        <div class="modal-overlay">
+        <div class="modal-overlay" data-close="true">
             <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
                 <div class="modal-header">
                     <span class="modal-title">${options.title || 'Window'}</span>
-                    ${options.closable ? `<span class="modal-close">&times;</span>` : ''}
+                    ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
                 </div>
                 <div class="modal-body">${options.content || ''}</div>
                 <div class="modal-footer">
@@ -25,20 +25,37 @@ $.modal = function (options) {
     const ANIMATION_SPEED = 200
     const $modal = _createModal(options)
     let closing = false
+    let destroyed = false
 
-    return {
-        open () {
-            !closing && $modal.classList.add('open')
-        },
-        close() {
-            closing = true
-            $modal.classList.remove('open')
-            $modal.classList.add('hide')
-            setTimeout(() => {
-                $modal.classList.remove('hide')
-                closing = false
-            }, ANIMATION_SPEED)
-        },
-        destroy() {}
+const modal = {
+    open () {
+        if (destroyed) {
+            return console.log('Modal is destroyed')
+        }
+        !closing && $modal.classList.add('open')
+    },
+    close() {
+        closing = true
+        $modal.classList.remove('open')
+        $modal.classList.add('hide')
+        setTimeout(() => {
+            $modal.classList.remove('hide')
+            closing = false
+        }, ANIMATION_SPEED)
     }
+}
+    const listener = event => {
+        if (event.target.dataset.close) {
+            modal.close()
+        }
+    }
+
+    $modal.addEventListener('click', listener)
+    return Object.assign(modal, {
+        destroy() {
+            $modal.parentNode.removeChild($modal)//Удаление ноды из DOM дерева
+            $modal.removeEventListener('click', listener)
+            destroyed = true
+        }
+    })
 }
